e636a27593503a914118b96d56d7c6cbd256ed2b
Carla <csachett@student.scf.edu>
2022-07-26 16:33:13 -0400

    Completed asignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("#create-task-form");
+  const label = document.getElementsByName("#new-task-description");
+  const input = document.getElementById("#new-task-priority");
+  const ul = document.getElementById("tasks");
+form.addEventListener("submit", (event) => {
+  
+  event.preventDefault();
+  form.requestFullscreen();
+})
+  
 });
