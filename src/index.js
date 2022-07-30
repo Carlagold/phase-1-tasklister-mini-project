fd464bfb69eb66dc5c739da0b76610e54ad7eeb1
Carla <csachett@student.scf.edu>
2022-07-30 11:11:36 -0400

    Complete assignmet

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  const form = document.getElementById("#create-task-form");
-  const label = document.getElementsByName("#new-task-description");
-  const input = document.getElementById("#new-task-priority");
-  const ul = document.getElementById("tasks");
-form.addEventListener("submit", (event) => {
-  
+ const form = document.querySelector('#create-task-form')
+// Issue when we click the submit botton the form triggers a refresh
+
+form.addEventListener('submit', (event) => {
   event.preventDefault();
-  form.requestFullscreen();
+  const taskValue = event.target['new-task-description'].value
+
+// find the ul so we can append
+const ul = document.querySelector('#task')
+const li = document.createElement('li')
+
+li.textContent = taskValue
+ul.append(li)
 })
-  
 });
